63ca4a8bb9089cbc0663435b85efd7b3d3e9b7b5
Binny <ibinstock@outlook.com>
2023-04-26 12:48:45 -0800

    finished all the tweaking for the page layout and design

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -16,10 +16,14 @@ function relocateTab(oldTab, newTab) {
     var cleared = document.querySelector(`#${newTab}`).classList.contains('cleared');
     document.querySelector(`#${oldTab}`).className = "hide cleared";
     document.querySelector(`#${newTab}`).className = "start";
+    document.querySelectorAll(`a[href="#${newTab}"]`)[0].className = "tab";
     currentTab = document.querySelector(`#${newTab}`);
     if (cleared) {
         // do nothing
     } else {
+        for (let i = 0; i < document.querySelectorAll('.tab').length; i++) {
+            document.querySelectorAll('.tab')[i].style.pointerEvents = "none";
+        }
         myFunction();
     }
 }
@@ -33,7 +37,6 @@ for(i = 0; i < switchTab.length; i++){
         var oldTab = currentTab.getAttribute("id");
         // Relocate to desired tab
         console.log(`${oldTab}, ${newTab}`);
-        clearTimeout(timerId);
         relocateTab(oldTab, newTab);
     })
 };
